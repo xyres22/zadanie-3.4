0b114a47baba3e15826fa455a5c2cdeaaacdf47e
Piotr Sygnatowicz <piotrek.lubin12@gmail.com>
2021-09-22 12:30:09 +0200

    Add loop and print

--- zakupy.py
+++ zakupy.py
@@ -2,3 +2,9 @@ zakupy = {
     'Piekarnia': ['Chleb', 'Bułka', 'Pączek'],
     'Warzywniak': ['Marchew', 'Seler', 'Rukola']
 }
+print('Lista zakupów')
+g = sum(len(val) for val in zakupy.values())
+for i,v in zakupy.items():
+    print(f'Idę do {i}, kupuję tu następujące rzeczy: {v}.')
+
+print(f'W sumie kupuję {g} produktów.')
