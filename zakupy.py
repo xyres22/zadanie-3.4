d3345c9043d9f44f0514c0bc39e19c2638fc72c1
Piotr Sygnatowicz <piotrek.lubin12@gmail.com>
2021-09-22 12:32:16 +0200

    Add upper case for values and keys

--- zakupy.py
+++ zakupy.py
@@ -2,9 +2,11 @@ zakupy = {
     'Piekarnia': ['Chleb', 'Bułka', 'Pączek'],
     'Warzywniak': ['Marchew', 'Seler', 'Rukola']
 }
+zakupy_upper = {str(k).upper():str(v).upper()   for k,v in zakupy.items()}
 print('Lista zakupów')
 g = sum(len(val) for val in zakupy.values())
 for i,v in zakupy.items():
     print(f'Idę do {i}, kupuję tu następujące rzeczy: {v}.')
 
 print(f'W sumie kupuję {g} produktów.')
+
